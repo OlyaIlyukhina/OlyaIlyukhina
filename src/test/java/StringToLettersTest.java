6b450e9f202dcc649938a602ffd6772e5bb0130b
OlyaIlyukhina <olya.ilyukhina@gmail.com>
2022-05-29 11:40:21 -0400

    More algorithms are coming

--- src/test/java/StringToLettersTest.java
+++ src/test/java/StringToLettersTest.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringToLettersTest {
+
+    @Test
+    public void testStringToLettersHappyPath() {
+        String s = "91839hddjfbieuty394  38424R";
+        String expectedResult = "hddjfbieutyR";
+
+        StringToLetters stringToLetters = new StringToLetters();
+        String actualResult = stringToLetters.stringToLettersAlgorithm(s);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testStringToLettersHappyPathEmpty() {
+        String s = "";
+        String expectedResult = "";
+
+        StringToLetters stringToLetters = new StringToLetters();
+        String actualResult = stringToLetters.stringToLettersAlgorithm(s);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testStringToLettersHappyPathNoLetters() {
+        String s = "123571625415484 37174     748364=]";
+        String expectedResult = "";
+
+        StringToLetters stringToLetters = new StringToLetters();
+        String actualResult = stringToLetters.stringToLettersAlgorithm(s);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
 }
